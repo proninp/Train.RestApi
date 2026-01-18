2b4956b0a28adb9c6883b4f2fe44572b65b36c68
proninp <velninpapro@gmail.com>
2026-01-18 14:00:24 +0300

    Added repositor layer

---
+++ Train.RestApiCourse/Movies.Application/Database/DbInitializer.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+
+namespace Movies.Application.Database;
+
+public sealed class DbInitializer
+{
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+
+    public DbInitializer(IDbConnectionFactory dbConnectionFactory)
+    {
+        _dbConnectionFactory = dbConnectionFactory;
+    }
+
+    public async Task InitializeAsync(CancellationToken cancellationToken = default)
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync(cancellationToken);
+
+        await connection.ExecuteAsync("""
+                                          create table if not exists movies (
+                                          id UUID primary key,
+                                          slug TEXT not null, 
+                                          title TEXT not null,
+                                          yearofrelease integer not null);
+                                      """);
+        
+        await connection.ExecuteAsync("""
+                                          create unique index concurrently if not exists movies_slug_idx
+                                          on movies
+                                          using btree(slug);
+                                      """);
+        
+        await connection.ExecuteAsync("""
+                                          create table if not exists genres (
+                                          movieId UUID references movies (Id),
+                                          name TEXT not null);
+                                      """);
+    }
+}
---
+++ Train.RestApiCourse/Movies.Application/Database/NpgsqlConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using Npgsql;
+
+namespace Movies.Application.Database;
+
+public sealed class NpgsqlConnectionFactory : IDbConnectionFactory
+{
+    public readonly string _connectionString;
+
+    public NpgsqlConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public async Task<IDbConnection> CreateConnectionAsync(CancellationToken cancellationToken = default)
+    {
+        var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync(cancellationToken);
+        return connection;
+    }
+}
