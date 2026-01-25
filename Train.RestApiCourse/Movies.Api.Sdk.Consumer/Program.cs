c5c68cdfe10cb4bf02fb436cd59492db4e31d2d1
proninp <velninpapro@gmail.com>
2026-01-25 14:43:08 +0300

    Added HttpClientFactory

--- Train.RestApiCourse/Movies.Api.Sdk.Consumer/Program.cs
+++ Train.RestApiCourse/Movies.Api.Sdk.Consumer/Program.cs
@@ -1,9 +1,30 @@
 ﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
+using Movies.Contracts.Requests;
 using Refit;
 
-var moviesApi = RestService.For<IMoviesApi>("http://localhost:5001");
+var services = new ServiceCollection();
 
-var movie = moviesApi.GetMoviesAsync("jumanji-1995");
+services.AddRefitClient<IMoviesApi>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:5001"));
 
-Console.WriteLine(JsonSerializer.Serialize(movie));
+var provider = services.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
+
+var getAllRequest = new GetAllMoviesRequest
+{
+    SortBy = null,
+    Title = null,
+    Year = null,
+    Page = 1,
+    PageSize = 3
+};
+
+var movies = await moviesApi.GetMoviesAsync(getAllRequest);
+
+foreach (var movie in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movie));
+}
