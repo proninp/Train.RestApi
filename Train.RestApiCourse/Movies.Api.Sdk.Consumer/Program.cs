92df10a6b22012dccf54eacbd7e61b162990ec68
proninp <velninpapro@gmail.com>
2026-01-25 09:47:05 +0300

    Created an SDK for the API

---
+++ Train.RestApiCourse/Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json;
+using Movies.Api.Sdk;
+using Refit;
+
+var moviesApi = RestService.For<IMoviesApi>("http://localhost:5001");
+
+var movie = moviesApi.GetMoviesAsync("jumanji-1995");
+
+Console.WriteLine(JsonSerializer.Serialize(movie));
